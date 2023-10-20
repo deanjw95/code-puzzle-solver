f64581d13bfa7a43731e7473861ae7826a859935
Jeong Wook Byun <deanjw95@naver.com>
2023-10-20 12:53:07 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ src/programmers/lv1/x만큼간격이있는n개의숫자.java
@@ -0,0 +1,17 @@
+package src.programmers.lv1;
+
+import java.util.Stack;
+
+public class x만큼간격이있는n개의숫자 {
+    class Solution {
+        public long[] solution(int x, int n) {
+            long[] answer = new long[n];
+
+            for (int i = 0; i < n; i++) {
+                answer[i] = (long) x * (i + 1);
+            }
+
+            return answer;
+        }
+    }
+}
