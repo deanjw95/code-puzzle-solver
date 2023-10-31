9040470aa49e357640275a56b104ebdfee7244e7
Jeong Wook Byun <deanjw95@naver.com>
2023-10-31 13:20:03 +0900

    직사각형 별 찍기

---
+++ src/programmers/lv1/직사각형별찍기.java
@@ -0,0 +1,20 @@
+package src.programmers.lv1;
+
+import java.util.Scanner;
+
+public class 직사각형별찍기 {
+    class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            for (int i = 0; i < b; i++) {
+                for (int j = 0; j < a; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
