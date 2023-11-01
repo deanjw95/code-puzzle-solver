5e8f7b4e637fc88d4d348d6906c859adfb3ae622
Jeong Wook Byun <deanjw95@naver.com>
2023-11-01 16:29:20 +0900

    자연수 뒤집어 배열로 만들기

---
+++ src/programmers/lv1/자연수뒤집어배열로만들기.java
@@ -0,0 +1,17 @@
+package src.programmers.lv1;
+
+public class 자연수뒤집어배열로만들기 {
+    class Solution {
+        public int[] solution(long n) {
+            String s = String.valueOf(n);
+            int[] answer = new int[s.length()];
+
+            for (int i = 0; i < s.length(); i++) {
+                answer[i] = s.charAt(s.length() - 1 - i) - '0';
+            }
+
+            return answer;
+        }
+    }
+}
+k
