6edadc18215745f346b5c89e570c1a6951df320f
Jeong Wook Byun <deanjw95@naver.com>
2023-11-10 10:22:50 +0900

    문자열을 정수로 바꾸기

---
+++ src/programmers/lv1/문자열을정수로바꾸기.java
@@ -0,0 +1,9 @@
+package src.programmers.lv1;
+
+public class 문자열을정수로바꾸기 {
+    class Solution {
+        public int solution(String s) {
+                return Integer.parseInt(s);
+        }
+    }
+}
