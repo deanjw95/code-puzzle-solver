779c0d57911f3f652e85dcb0e4028198a21a1404
Jeong Wook Byun <deanjw95@naver.com>
2023-11-15 11:22:25 +0900

    두 정수 사이의 합

---
+++ src/programmers/lv1/두정수사이의합.java
@@ -0,0 +1,15 @@
+package src.programmers.lv1;
+
+public class 두정수사이의합 {
+    class Solution {
+        public long[] solution(int x, int n) {
+            long[] answer = new long[n];
+
+            for (int i = 0; i < n; i++) {
+                answer[i] = (long) x * (i + 1);
+            }
+
+            return answer;
+        }
+    }
+}
