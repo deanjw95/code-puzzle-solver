83e6a9038151603b5eeefb85d5333a94692f23e8
Jeong Wook Byun <deanjw95@naver.com>
2023-12-07 10:33:00 +0900

    마법의 엘리베이터

---
+++ src/programmers/lv2/마법의엘리베이터.java
@@ -0,0 +1,94 @@
+package src.programmers.lv2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 마법의엘리베이터 {
+    class Solution {
+        public int solution(int storey) {
+            int answer = 0;
+
+            Queue<Storey> q = new LinkedList<>();
+            q.add(new Storey(storey, 0));
+
+
+
+            return answer;
+        }
+    }
+
+    class Storey {
+        int floor;
+        int level;
+
+        public Storey(int storey, int i) {
+            this.floor = storey;
+            this.level = i;
+        }
+    }
+
+    //마법의 엘리베이터
+
+//절댓값이 10 ^ c ( c >= 0 인 정수)
+//더한 값으로 이동
+//0보다 작으면 엘리베이터는 움직이지 않습니다.
+//0층이 가장 아래층 -> 엘리베이터 와 민수 모두 0층
+
+//절반보다 크면 더해서 큰 값을 만드는게 이득
+//절반보다 작으면 빼서 작은 값을 만드는게 이득
+
+    class Solution2 {
+        public int solution(int storey) {
+            int answer = 0;
+
+            String stVal = String.valueOf(storey);
+            char[] chars = stVal.toCharArray();
+            int[] ints = new int[chars.length];
+            for (int i = 0; i < chars.length; i++) {
+                ints[i] = chars[i] - '0';
+            }
+
+            for (int i = ints.length - 1; i >= 0; i--) {
+                int anInt = ints[i];
+
+                if (anInt == 10) {
+                    if (i - 1 < 0) {
+                        answer += 1;
+                        continue;
+                    }
+
+                    ints[i - 1]++;
+                    continue;
+                }
+
+                if (anInt >= 6) {
+                    answer += (10 - anInt);
+                    if (i - 1 < 0) {
+                        answer += 1;
+                        continue;
+                    }
+                    ints[i - 1]++;
+                } else if (anInt <= 4) {
+                    answer += anInt;
+                } else if (anInt == 5) {
+                    if (i - 1 < 0) {
+                        answer += 5;
+                        continue;
+                    } else {
+                        if (ints[i - 1] < 5) {
+                            answer += anInt;
+                        } else {
+                            answer += 5;
+                            ints[i - 1]++;
+                        }
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+
+    /*
+    * */
+}
